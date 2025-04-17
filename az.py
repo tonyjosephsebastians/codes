8b8fff5a6225e7bb1d4285bfe30d0c3607ffebdc
TONY JOSEPH SEBASTIAN <37629628+tonyjosephsebastians@users.noreply.github.com>
2025-04-16 23:07:16 -0400

    Update az.py

--- az.py
+++ az.py
@@ -1,3 +1,6 @@
+ACCESS_TOKEN=$(az account get-access-token --resource https://cognitiveservices.azure.com/ --query accessToken -o tsv)
+
 az rest --method get \
-    --url https://<your-openai-endpoint>.openai.azure.com/openai/models?api-version=2024-10-21 \
-    --headers "Authorization=Bearer $(az account get-access-token --query accessToken -o tsv)" 
+  --url "https://<your-openai-endpoint>.openai.azure.com/openai/models?api-version=2024-10-21" \
+  --headers "Authorization=Bearer $ACCESS_TOKEN"
+
